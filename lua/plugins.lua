a88dc79ed66c1267446ca6bccbd0e9e764cd3174
turato <43437788+hustuhao@users.noreply.github.com>
2024-06-16 14:51:57 +0800

    feat: add telescope plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -68,6 +68,12 @@ return require('packer').startup(function(use)
         use 'github/copilot.vim'
         use 'lukas-reineke/indent-blankline.nvim'
 
+        use {
+            'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        -- or                            , branch = '0.1.x',
+            requires = { {'nvim-lua/plenary.nvim'} }
+        }
+
         -- Automatically set up your configuration after cloning packer.nvim
         -- Put this at the end after all plugins
         if packer_bootstrap then
