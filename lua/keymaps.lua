a1eaf0468bd3fd2151ea5973ec8ea5c451ec4207
tuhao <tuhao@weipie.com>
2024-06-21 20:12:07 +0800

    feat: add telescope keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -15,3 +15,12 @@ map('n', '<leader>te', '<Esc>:FloatermToggle<CR>', opts)
 map('n', '<leader>g', '<Esc>:CocCommand git.showBlameDoc<CR>', opts)
 map('v', '<C-c>', '"+y', opts)
 map('n', '<C-v>', '"*p', opts)
+
+--按键设置
+vim.api.nvim_set_keymap("n", "<leader>ff", [[<cmd>lua require('telescope.builtin').find_files()<cr>]], {})
+vim.api.nvim_set_keymap("n", "<leader>fg", [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], {})
+vim.api.nvim_set_keymap("n", "<leader>fb", [[<cmd>lua require('telescope.builtin').buffers()<cr>]], {})
+vim.api.nvim_set_keymap("n", "<leader>fh", [[<cmd>lua require('telescope.builtin').help_tags()<cr>]], {})
+vim.api.nvim_set_keymap("n", "<leader>ma", [[<cmd>Telescope vim_bookmarks current_file<cr>]], {})
+vim.api.nvim_set_keymap("n", "<leader>mA", [[<cmd>Telescope vim_bookmarks all<cr>]], {})
+vim.api.nvim_set_keymap("n", "<leader>fo", [[<cmd>Telescope oldfiles theme=dropdown previewer=false<cr>]], {})
